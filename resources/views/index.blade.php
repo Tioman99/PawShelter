b0b1223b69531112c03fcaf55f580a075b473859
Tioman99 <hluc626@gamil.com>
2023-12-17 23:57:34 +0100

    migration?
    done?

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testicle</title>
+</head>
+<body>
+    <h1>Beanss</h1>
+</body>
+</html>
